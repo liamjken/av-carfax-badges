8e37bd431976f530fa34e985c1168e6c09f70c83
liamjken <liamjken@gmail.com>
2023-01-25 13:35:10 -0500

    starting point

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       AV Carfax Badges
+ * Plugin URI:        https://autoverify.com
+ * Description:       This plugin is intended to integartate the Carfax badging API with the inventory.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Liam Kennedy
+ * Author URI:        https://autoverify.com
+ * Text Domain:       av-carfax-badges
+ * Domain Path:       /languages
+ */
